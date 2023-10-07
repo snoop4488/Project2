59a58b82bd73e72b212ea17eb981b7c8753a54f7
snoop4488 <143089036+snoop4488@users.noreply.github.com>
2023-10-07 15:31:19 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -9,8 +9,7 @@ st.title("Reconocimiento óptico de Caracteres")
 
 img_file_buffer = st.camera_input("Toma una Foto")
 
-with st.sidebar:
-      filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
+
 
 
 if img_file_buffer is not None:
