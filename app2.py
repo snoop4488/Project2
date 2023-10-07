bfa6daaeb0ce98f9c49dc0894807b32e0af68a0a
snoop4488 <143089036+snoop4488@users.noreply.github.com>
2023-10-07 15:47:30 -0500

    app2.py

--- app2.py
+++ app2.py
@@ -4,16 +4,13 @@ import numpy as np
 import pytesseract
 from PIL import Image
 
-
 st.title("Reconocimiento óptico de Caracteres")
 
+# Agregar la opción para cargar una imagen desde el sistema
 uploaded_image = st.file_uploader("Cargar una imagen", type=["jpg", "png", "jpeg"])
 
 # Agregar la opción para tomar una foto con la cámara
 img_file_buffer = st.camera_input("Tomar una foto")
-
-with st.sidebar:
-    filtro = st.radio("Aplicar Filtro", ('Con Filtro', 'Sin Filtro'))
 
 if uploaded_image is not None:
     # Si se cargó una imagen desde el sistema, la leemos y la procesamos
@@ -27,15 +24,6 @@ else:
     st.write("Cargue una imagen o tome una foto para continuar")
 
 if uploaded_image is not None or img_file_buffer is not None:
-    if filtro == 'Con Filtro':
-        cv2_img = cv2.bitwise_not(cv2_img)
-    else:
-        cv2_img = cv2_img
-
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
     text = pytesseract.image_to_string(img_rgb)
     st.write(text)
-
-
-    
-
