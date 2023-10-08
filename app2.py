45e3e8a6bbe0167050bceda5d22d878678adddfe
snoop4488 <143089036+snoop4488@users.noreply.github.com>
2023-10-08 16:08:20 -0500

    app2.py

--- app2.py
+++ app2.py
@@ -14,22 +14,19 @@ uploaded_image = st.file_uploader("Cargar una imagen", type=["jpg", "png", "jpeg
 # Agregar la opción para tomar una foto con la cámara
 img_file_buffer = st.camera_input("Tomar una foto")
 
+# Variable para almacenar el texto reconocido por OCR
+ocr_text = ""
+
 if uploaded_image is not None:
     # Si se cargó una imagen desde el sistema, la leemos y la procesamos
     pil_image = Image.open(uploaded_image)
     cv2_img = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
+    img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+    ocr_text = pytesseract.image_to_string(img_rgb)
 elif img_file_buffer is not None:
     # Si se tomó una foto con la cámara, la procesamos
     bytes_data = img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-else:
-    cv2_img = None
-
-# Variable para almacenar el texto reconocido por OCR
-ocr_text = ""
-
-if cv2_img is not None:
-    # Si hay una imagen, realizar OCR y almacenar el texto
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
     ocr_text = pytesseract.image_to_string(img_rgb)
 
